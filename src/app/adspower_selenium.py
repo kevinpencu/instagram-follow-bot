c78f2a3add798dc2515816089bb1c262a3e523c5
Hussein Siblani <hussein@dyncep.com>
2025-07-26 10:04:26 +0900

    adspower_selenium.py: Wrap for error handling

--- src/app/adspower_selenium.py
+++ src/app/adspower_selenium.py
@@ -7,20 +7,25 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from app.selenium_utils import close_tabs
 from app.adspower.api_wrapper import StartProfileResponse
+from app.logger import get_logger
 
 
 def run_selenium(
     start_profile_response: StartProfileResponse,
 ) -> webdriver.Chrome:
-    options = Options()
-    options.add_experimental_option(
-        "debuggerAddress",
-        f"127.0.0.1:{start_profile_response.debug_port}",
-    )
+    try:
+        options = Options()
+        options.add_experimental_option(
+            "debuggerAddress",
+            f"127.0.0.1:{start_profile_response.debug_port}",
+        )
 
-    service = Service(start_profile_response.webdriver)
+        service = Service(start_profile_response.webdriver)
 
-    driver = webdriver.Chrome(service=service, options=options)
-    close_tabs(driver)
+        driver = webdriver.Chrome(service=service, options=options)
+        close_tabs(driver)
 
-    return driver
+        return driver
+    except Exception as e:
+        get_logger().error(f"[ADSPOWER-SELENIUM]: Failed to create WebDriver: {str(e)}")
+        raise
