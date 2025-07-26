13afe0a29a1597605445e2d19b37fb6118093e61
Hussein Siblani <hussein@dyncep.com>
2025-07-26 10:10:28 +0900

    selenium_utils.py: Enable back close tabs

--- src/app/selenium_utils.py
+++ src/app/selenium_utils.py
@@ -3,7 +3,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 def close_tabs(driver: webdriver.Chrome):
-    return
     windows = driver.window_handles
     if len(windows) == 1:
         return
@@ -11,6 +10,8 @@ def close_tabs(driver: webdriver.Chrome):
     for i in range(1, len(windows)):
         driver.switch_to.window(windows[i])
         driver.close()
+
+    driver.switch_to.window(driver.window_handles[0])
 
 
 def navigate_to(driver: webdriver.Chrome, url: str):
