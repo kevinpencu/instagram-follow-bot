ba7b214d93eea4ebae5d141c54fa4acb8b45a5dc
Hussein Siblani <hussein@dyncep.com>
2025-08-04 04:28:27 +0900

    logger.py: Write logs to 'app.log'

--- src/app/logger.py
+++ src/app/logger.py
@@ -1,9 +1,23 @@
 import logging
 import inspect
+import sys
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+log_file = 'app.log'
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+file_handler = logging.FileHandler(log_file)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logging.getLogger().addHandler(file_handler)
+logging.getLogger().setLevel(logging.DEBUG)
+
+sys.stdout = open(log_file, 'a')
+sys.stderr = open(log_file, 'a')
 
 
 def get_logger():
